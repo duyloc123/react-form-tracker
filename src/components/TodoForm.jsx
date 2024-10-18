0ff21716dd77c1df1428dd4fbeb740284f6e6e93
duyloc123 <loc123456000@gmail.com>
2024-10-18 23:49:37 +0700

    Validation

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -9,30 +9,74 @@ function TodoForm({ addIssueTracker }) {
     orderyBy: '',
     searchBy: ''
   })
+  const [isSubmitted, setIsSubmitted] = React.useState(false)
 
   function onChangeForm(e) {
     const { name, value } = e.target;
-    setForm(prevState => {
-      return {
-        ...prevState,
-        [name]: value
-      }
-    })
+    setForm(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  // Validation
+  function getFieldError(field, value) {
+    switch (field) {
+      case 'title':
+        if (value.trim() === '' || value.length < 13) {
+          return 'Title must not be blank and must be at least 13 characters long';
+        }
+        break;
+      case 'author':
+        if (value === '') {
+          return "Must not leave the author's name blank";
+        }
+        break;
+      case 'severity':
+        if (value === '') {
+          return "Must not leave the serverity blank";
+        }
+        break;
+      case 'description':
+        if (value.trim() === '' || value.length < 30) {
+          return 'Description must not be blank and must be at least 13 characters long';
+        }
+        break;
+      default:
+        return '';
+    }
+    return '';
+  }
+
+  function hasErrors() {
+    return Object.keys(form).some(key => getFieldError(key, form[key]) !== '');
   }
 
   function submit(e) {
     e.preventDefault();
-    const { title, author, severity, description } = form;
-    const item = {
-      // id: issueTracker.length + 1,
-      id: Date.now(),
-      title,
-      author,
-      severity,
-      description,
-      status: "new",
-    };
-    addIssueTracker(item);
+    setIsSubmitted(true);
+    if(!hasErrors()){
+      const { title, author, severity, description } = form;
+      const item = {
+        id: Date.now(),
+        title,
+        author,
+        severity,
+        description,
+        status: "new",
+      };
+      addIssueTracker(item);
+      
+      setForm({
+        title: '',
+        author: '',
+        severity: '',
+        description: '',
+        orderyBy: '',
+        searchBy: ''
+      });
+      setIsSubmitted(false);
+    } return;
   }
 
   return (
@@ -43,11 +87,13 @@ function TodoForm({ addIssueTracker }) {
         id="issueTitle"
         name="title"
         value={form.title}
-        className="mt-3 mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
+        className="mt-3 mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         placeholder="Title..."
         required
         onChange={onChangeForm}
       />
+      {isSubmitted && getFieldError('title', form.title) && 
+        <p className="text-red-500 text-xs mb-3">{getFieldError('title', form.title)}</p>}
 
       <label htmlFor="issueAuthor">Author</label>
       <select
@@ -55,28 +101,31 @@ function TodoForm({ addIssueTracker }) {
         name="author"
         value={form.author}
         onChange={onChangeForm}
-        class="mt-3 mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
+        className="mt-3 mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
       >
-        <option selected className="text-gray-300">
-          Choose author...
-        </option>
+        <option value="">Choose author...</option>
         <option value="tony">Tony</option>
         <option value="loc">Loc</option>
         <option value="hoa">Hoa</option>
       </select>
+      {isSubmitted && getFieldError('author', form.author) && 
+        <p className="text-red-500 text-xs mb-3">{getFieldError('author', form.author)}</p>}
+
       <label htmlFor="issueSeverity">Severity</label>
       <select
         id="issueSeverity"
         name="severity"
         value={form.severity}
         onChange={onChangeForm}
-        className="mt-3 mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
+        className="mt-3 mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
       >
-        <option selected>Choose severity...</option>
+        <option value="">Choose severity...</option>
         <option value="Low">Low</option>
         <option value="Medium">Medium</option>
         <option value="High">High</option>
       </select>
+      {isSubmitted && getFieldError('severity', form.severity) && 
+        <p className="text-red-500 text-xs mb-3">{getFieldError('severity', form.severity)}</p>}
 
       <label htmlFor="issueDescription">Description</label>
       <textarea
@@ -85,13 +134,15 @@ function TodoForm({ addIssueTracker }) {
         name="description"
         value={form.description}
         onChange={onChangeForm}
-        class="block mt-3 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500   "
+        className="block mt-3 mb-1 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
         placeholder="Write your thoughts here..."
       />
+      {isSubmitted && getFieldError('description', form.description) && 
+        <p className="text-red-500 text-xs mb-3">{getFieldError('description', form.description)}</p>}
 
-      <div className=" w-full inline-block mb-4 mt-4 ">
+      <div className="w-full inline-block mb-4 mt-4">
         <button
-          className=" float-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-300 ease-linear"
+          className="float-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-300 ease-linear"
           onClick={submit}
         >
           Add
